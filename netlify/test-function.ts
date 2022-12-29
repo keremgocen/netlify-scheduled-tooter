72201d8e63cb7333db99f1d9ff294d7afed4a4fd
keremgocen <keremgocen@gmail.com>
2022-12-29 02:43:36 +0000

    more filtering, sorcery

--- netlify/test-function.ts
+++ netlify/test-function.ts
@@ -22,43 +22,59 @@ const handler: Handler = async (
   //  max_id = ( int( today_end.timestamp() )     << 16 ) * 1000
   //  min_id = ( int( yesterday_end.timestamp() ) << 16 ) * 1000
   const date = new Date();
-  const dateRep =
+  const yesterdayBigInt =
     BigInt(date.setTime(date.getTime() - 1000 * 24 * 3600)) << 16n;
-  const today: string = dateRep.toString();
+  const yesterday = yesterdayBigInt.toString();
+
+  const publicVis: mastodon.v1.StatusVisibility = "public";
 
   const statuses: mastodon.v1.Status[] = await masto.v1.accounts.listStatuses(
     process.env.USER_ID ?? "",
     {
       excludeReplies: true,
       excludeReblogs: true,
-      minId: today,
+      sinceId: yesterday, // todo make this flexible
     }
   );
 
   console.log(statuses.length);
   console.log(
     "mindId",
-    today.toString(),
+    yesterday,
     date.setTime(date.getTime() - 1000 * 24 * 3600)
   );
 
   let i = 0;
   for (const s of statuses) {
-    console.log("status createdAt", i++, s.createdAt, s.id, s.visibility);
-  }
-
-  const publicStatuses = statuses.filter((s) => s.visibility === "public");
-  for (const s of publicStatuses) {
     console.log(
-      "public status createdAt",
+      "status",
+      i++,
       s.createdAt,
-      s.id,
       s.visibility,
+      s.inReplyToAccountId,
+      s.inReplyToId,
       s.content
     );
   }
 
-  // todo tweet these now
+  // const publicStatuses = statuses.filter(
+  //   (s) =>
+  //     s.visibility === publicVis &&
+  //     s.inReplyToId === null &&
+  //     s.inReplyToAccountId === null
+  // );
+  // for (const s of publicStatuses) {
+  //   console.log(
+  //     "public status createdAt",
+  //     s.createdAt,
+  //     s.id,
+  //     s.visibility,
+  //     s.content
+  //   );
+  // }
+
+  // todo tweet now
+  // maybe try to generate unique id for twitter lookup so we don't tweet duplicate
 
   return {
     statusCode: 200,
